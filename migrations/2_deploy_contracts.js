b3780a7b273f75b44dd97cc624e35d67df7c326a
Vitali <vitali.repin@gmail.com>
2021-01-24 20:52:18 +0100

    Deploy Contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  //Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  //Deploy EthSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  // Trasnfer all tokens to EthSwap (1 million)
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
 };
