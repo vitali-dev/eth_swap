28f34d1410d66616c69fd1297410a9cd8f550b80
Vitali <vitali.repin@gmail.com>
2021-01-25 19:10:43 +0100

    Fixed error 'react-bootstrap/lib/Breadcrumb'

---
+++ test/EthSwap.test.js
@@ -0,0 +1,19 @@
+const { assert } = require("chai");
+const { Item } = require("react-bootstrap/lib/Breadcrumb");
+
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("EthSwap", (accounts) => {
+  describe("EthSwap deployment", async () => {
+    it("contract has a name", async () => {
+      let ethSwap = await EthSwap.new();
+      const name = await ethSwap.name();
+      assert.equal(name, "EthSwap Instant Exchange");
+    });
+  });
+});
