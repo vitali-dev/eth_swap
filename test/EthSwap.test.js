b6f30ebf2d481a4b7b992af684cd913ccd8d73f1
Vitali <vitali.repin@gmail.com>
2021-01-25 19:44:03 +0100

    Create helper function to convet Wei to Ether

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,5 +1,6 @@
 const { assert } = require("chai");
 const { Item } = require("react-bootstrap/lib/Breadcrumb");
+const { default: Web3 } = require("web3");
 
 const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
@@ -8,12 +9,38 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
+function tokens(n) {
+  return web3.utils.toWei(n, "ether");
+}
+
 contract("EthSwap", (accounts) => {
+  let token;
+  let ethSwap;
+
+  before(async () => {
+    token = await Token.new();
+    ethSwap = await EthSwap.new();
+
+    // Trasnfer all tokens to EthSwap (1 million)
+    await token.transfer(ethSwap.address, tokens("1000000"));
+  });
+
+  describe("Token deployment", async () => {
+    it("contract has a name", async () => {
+      const name = await token.name();
+      assert.equal(name, "DApp Token");
+    });
+  });
+
   describe("EthSwap deployment", async () => {
     it("contract has a name", async () => {
-      let ethSwap = await EthSwap.new();
       const name = await ethSwap.name();
       assert.equal(name, "EthSwap Instant Exchange");
+    });
+
+    it("contract has tokens", async () => {
+      let balance = await token.balanceOf(ethSwap.address);
+      assert.equal(balance.toString(), tokens("1000000"));
     });
   });
 });
